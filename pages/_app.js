48f0c163b4e5e81bbdff6bf5cd2d3488ddd41163
Khyle Walker <khylewalker8@gmail.com>
2025-09-15 00:51:06 +0000

    Removed pages router to fix Next.js conflict

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-// pages/_app.js
-import "../firebaseConfig"; // initialize firebase once
-import "../styles/globals.css"; // keep global styles if you have them
-
-export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
